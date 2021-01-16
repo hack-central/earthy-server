f81190b72f74ad0c4bb9ff2c97addb12c3c54ba9
Karan Sanjeev <yo@alphaman.me>
2021-01-16 12:58:00 +0530

    Integrate whole logic

--- server.js
+++ server.js
@@ -1,28 +1,16 @@
 const jsonServer = require('json-server');
-const generator = require('./generator');
-const router = require('./routes');
+const customRouter = require('./routes');
 const consola = require('consola');
-const fs = require('fs');
 
 consola.LogLevel = 4;
 const server = jsonServer.create();
 const middlewares = jsonServer.defaults();
 
+const db = process.env.NODE_ENV === 'development' ? 'dev_db.json' : 'db.json';
+const router = server.router(db);
+
 server.use(middlewares);
+server.use(customRouter);
 server.use(router);
 
-const generateDevBuild = () => {
-    const mode = process.env.NODE_ENV;
-    const data = generator(mode);
-    consola.success('Generation completed. Populating latest json into db.json');
-
-    fs.writeFile(
-        mode === 'dev' ? 'dev_db.json' : 'db.json',
-        JSON.stringify(data),
-        'utf8',
-        () => consola.success('db.json populated!')
-    );
-};
-
-generateDevBuild();
 server.listen(process.env.PORT, () => consola.success('Listening on port', process.env.PORT));
