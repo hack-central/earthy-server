13db223f121c1a6f823905c3eb7205bcb40a446b
Karan Sanjeev <yo@alphaman.me>
2021-01-17 14:32:02 +0530

    Run prettier fix

--- routes.js
+++ routes.js
@@ -1,23 +1,25 @@
 const jsonServer = require('json-server');
 
 const router = jsonServer.create();
-const middlewares = jsonServer.defaults()
+const middlewares = jsonServer.defaults();
 
 router.use(jsonServer.bodyParser);
 router.use(middlewares);
-router.use(jsonServer.rewriter({
-    "/posts": "/posts?_embed=comments",
-    "/posts/:id": "/posts/:id?_embed=comments",
-    "/users": "/users?_embed=posts&_embed=comments",
-    "/users/:id": "/users/:id?_embed=posts&_embed=comments"
-}));
+router.use(
+    jsonServer.rewriter({
+        '/posts': '/posts?_embed=comments',
+        '/posts/:id': '/posts/:id?_embed=comments',
+        '/users': '/users?_embed=posts&_embed=comments',
+        '/users/:id': '/users/:id?_embed=posts&_embed=comments',
+    })
+);
 router.use((req, res, next) => {
     if (req.method === 'POST') {
-      req.body.createdAt = new Date().toISOString();
+        req.body.createdAt = new Date().toISOString();
     }
     next();
 });
-  
+
 router.get('/ping', (req, res) => {
     return res.json({ message: 'Say my name', date: new Date() });
 });
